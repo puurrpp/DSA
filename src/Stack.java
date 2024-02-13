e5a42948e52e69d88272b700a531d813d44c30bf
Kyran Mendoza <mendozakyran8@gmail.com>
2024-02-13 16:05:59 +0800

    fully implement Stack<T>

--- src/Stack.java
+++ src/Stack.java
@@ -1,20 +1,41 @@
 public class Stack<T> {
 	Object[] _stack;
-	private int _len = -1;
+	private int top = -1;
+	private int maxSize = 1000;
 
+	Stack() {
+		_new();
+	}
 	Stack(int size) {
-		_stack = new Object[size];
+		maxSize = size;
+		_new();
+	}
+
+	private void _new() {
+		_stack = new Object[maxSize];
 	}
 
 	void push(T el) {
-		_stack[++_len] = el;
+		_stack[++top] = el;
 	}
 
 	T pop() {
-		return (T) _stack[_len--];
+		return (T) _stack[top--];
+	}
+
+	T peek() {
+		return (T) _stack[top];
 	}
 
 	boolean isEmpty() {
-		return _len == -1;
+		return top == -1;
+	}
+
+	boolean isFull() {
+		return top == (maxSize - 1);
+	}
+
+	void clear() {
+		top = -1;
 	}
 }
