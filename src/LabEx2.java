e2cfe90da5958b96a2fbfa8f7cc62295b7d9cd98
Kyran Mendoza <mendozakyran8@gmail.com>
2024-02-13 15:56:22 +0800

    implement LabEx2 Part 1

---
+++ src/LabEx2.java
@@ -0,0 +1,90 @@
+// Mendoza, Kyran Xandre O.
+// ICS2605 1CSA
+// Lab Exercise 2 Part 1
+
+import java.util.Scanner;
+
+public class LabEx2 {
+	static Scanner scanner = new Scanner(System.in);
+
+	static String postfixToInfix(String e) {
+
+		String[] ops = e.split(" ");
+
+		Stack<String> stuff = new Stack<>(ops.length);
+
+		for (String op : ops) {
+			if ("+-*/^".contains(op)) {
+				String b = stuff.pop();
+				String a = stuff.pop();
+
+				stuff.push("("+a+" "+op+" "+b+")");
+			} else {
+				stuff.push(op);
+			}
+		}
+
+		String output = "";
+
+		while (!stuff.isEmpty())
+			output += stuff.pop();
+
+		return output;
+	}
+
+	static double evalPostfix(String e) {
+		String[] ops = e.split(" ");
+
+		Stack<String> stuff = new Stack<>(ops.length);
+
+		for (String op : ops) {
+			if ("+-*/^".contains(op)) {
+				double b = Double.parseDouble(stuff.pop());
+				double a = Double.parseDouble(stuff.pop());
+
+				switch (op.charAt(0)) {
+					case '+':
+						stuff.push(String.valueOf(a + b));
+						break;
+					case '-':
+						stuff.push(String.valueOf(a - b));
+						break;
+					case '*':
+						stuff.push(String.valueOf(a * b));
+						break;
+					case '/':
+						stuff.push(String.valueOf(a / b));
+						break;
+					case '^':
+						stuff.push(String.valueOf(Math.pow(a, b)));
+				}
+			} else {
+				stuff.push(op);
+			}
+		}
+
+		return Double.parseDouble(stuff.pop());
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Bihasa, Christian A.");
+		System.out.println("Mendoza, Kyran Xandre O.");
+		System.out.println();
+
+		System.out.print("Input t: ");
+		int t = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.print("Input s: ");
+		String s = scanner.nextLine();
+		System.out.println();
+
+		if (t == 1) {
+			System.out.println("postfix: " + s);
+			System.out.println("infix: " + postfixToInfix(s));
+			System.out.println("eval: " + evalPostfix(s));
+		}
+	}
+
+
+}
