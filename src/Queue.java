1034ca074b1313c802a0f686ff953e96fbf5c36b
Kyran Mendoza <mendozakyran8@gmail.com>
2024-03-12 16:42:17 +0800

    fix Queue#toString

--- src/Queue.java
+++ src/Queue.java
@@ -1,11 +1,13 @@
-import java.util.Arrays;
+// Mendoza, Kyran Xandre O.
+// ICS2605 1CSA
+// Lab Exercise 3
 
 public class Queue<T> {
 	Object[] q;
 	private int front = -1;
 	private int rear = -1;
-
-	private int maxSize = 1000;
+	private int count = 0;
+	private int maxSize = 100;
 
 	Queue() {
 		q = new Object[maxSize];
@@ -14,44 +16,70 @@ public class Queue<T> {
 		if (size > 0) maxSize = size;
 		q = new Object[maxSize];
 	}
+	Queue(Queue<T> source) {
+		maxSize = source.maxSize;
+		q = source.q;
+		front = source.front;
+		rear = source.rear;
+	}
 
 	void enqueue(T el) {
+		if (isFull())
+			throw new IndexOutOfBoundsException("enqueue on full queue");
+
 		if (isEmpty())
 			front = 0;
-
 		rear = (++rear) % maxSize;
+		count++;
 		q[rear] = el;
 	}
 
 	T dequeue() {
+		if (isEmpty())
+			throw new IndexOutOfBoundsException("dequeue from empty queue");
+
 		T el = (T) q[front];
 		front = ++front % maxSize;
+		count--;
+		if (isEmpty()) clear();
 		return el;
 	}
 
 	T peek() {
+		if (isEmpty())
+			return null;
 		return (T) q[front];
 	}
 
 	boolean isEmpty() {
-		return front == -1 && front == rear;
+		return count == 0;
 	}
 
 	boolean isFull() {
-		return ((rear + 1) % maxSize) == front;
+		return count == maxSize;
 	}
 
 	void clear() {
 		front = rear = -1;
+		count = 0;
 	}
 
 	@Override
 	public String toString() {
-		return "Queue [max="+maxSize+"] {" +
-				"\nq= " + Arrays.toString(q) +
-				"\nfront= " + front + ", rear= " + rear +
-				"\nisFull= " + isFull() +
-				"\nisEmpty= " + isEmpty() +
-				"\n}";
+		StringBuilder output = new StringBuilder("Queue [max=" + maxSize + "] {" +
+				"\nq: [");
+
+		for (int i = 0; i < maxSize; i++)
+			output.append(q[i].toString()).append(",");
+
+		output
+				.append("]\nfront: ").append(front)
+				.append(", rear: ").append(rear)
+				.append("\ncount: ").append(count)
+				.append("\nisFull: ").append(isFull())
+				.append("\nisEmpty: ").append(isEmpty())
+				.append("\n}");
+
+		return output.toString();
 	}
 }
